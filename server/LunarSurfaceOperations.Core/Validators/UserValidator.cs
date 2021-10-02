71c871ad8a8b4f6285d99edb02f1d18e7347efda
MiroBG <moltenhardrock@abv.bg>
2021-10-02 13:17:03 +0300

    Removed excessive clutter

--- server/LunarSurfaceOperations.Core/Validators/UserValidator.cs
+++ server/LunarSurfaceOperations.Core/Validators/UserValidator.cs
@@ -7,7 +7,6 @@ using System.Web;
 using FluentValidation;
 using FluentValidation.Validators;
 using LunarSurfaceOperations.Data.Models;
-using Xunit;
 
 namespace LunarSurfaceOperations.Core.Validators
 {
@@ -19,11 +18,6 @@ namespace LunarSurfaceOperations.Core.Validators
             this.RuleFor(x => x.Email).NotNull().WithMessage("Please enter your email adress").EmailAddress().WithMessage("Please enter a valid email adress");
             this.RuleFor(x => x.Username).NotEmpty().WithMessage("Please make sure you have written your username.").Length(8, 20).WithMessage("Username must be between 8 and 20 symbols long.");
             this.RuleFor(x => x.Password).NotEmpty().WithMessage("Ensure you have written your password.").Length(8,32).WithMessage("Password must be between 8 and 32 symbols long.").NotEqual(x => x.Username).WithMessage("Password must not match Username.").NotEqual(x=> x.Email).WithMessage("Password must not match email");
-        }
-    
+        }  
     }
-
-
-
-
 }
