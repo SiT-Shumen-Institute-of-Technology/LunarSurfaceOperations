721b0e4edbe5a9adcc74391eaa999157dc2a9d32
Stoil Kostov <stoil_thebest@abv.bg>
2021-10-02 13:34:17 +0300

    Implemented Repositories For User & Workspace

---
+++ server/LunarSurfaceOperations.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+namespace LunarSurfaceOperations.Data.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using LunarSurfaceOperations.Data.Contracts;
+    using LunarSurfaceOperations.Data.Models;
+
+    interface IUserRepository : IRepository<User>
+    {
+    }
+}
